98fbcdc69f65327952738e8283546e519c34ea81
MarkoCodeLabs <marko.miletic.kt@gmail.com>
2026-06-02 17:34:41 +0200

    React Forms

---
+++ 05_ReactForms/src/App.jsx
@@ -0,0 +1,23 @@
+import TextInput from "./components/forms/TextInput";
+import ContactForm from "./components/forms/ContactForm";
+import RegistrationForm from "./components/forms/RegistrationForm";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <form>
+        <TextInput name="ime" label="Ime:" type="text" />
+        <TextInput name="email" label="Email:" type="email" />
+        <TextInput name="telefon" label="Telefon:" type="tel" />
+        <TextInput name="godine" label="Godine:" type="number" />
+      </form>
+      <h2>Contact Form</h2>
+      <ContactForm />
+      <h2>Registration Form</h2>
+      <RegistrationForm />
+    </>
+  );
+}
+
+export default App;
